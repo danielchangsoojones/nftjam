2031e27c37b6a0981dd797979d37b322aa7428c4
Daniel Jones <messyjones@gmail.com>
2022-03-17 16:33:46 -0700

    Initial Commit

---
+++ nftjam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nftjam
+//
+//  Created by Daniel Jones on 3/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

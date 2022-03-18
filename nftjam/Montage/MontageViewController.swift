571f02899cd2fd2d0baa8e4a66050cc4dabd2b0b
Daniel Jones <messyjones@gmail.com>
2022-03-17 18:39:12 -0700

    the img shows up

--- nftjam/Montage/MontageViewController.swift
+++ nftjam/Montage/MontageViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 class MontageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
-        let nftTag = NFTTagView(frame: CGRect(x: 0, y: 0, width: 74, height: 35))
+        self.view.backgroundColor = UIColor(red: 68.0 / 255, green: 64.0 / 255, blue: 175.0 / 255, alpha: 1)
+        let nftTag = ThumbnailView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         self.view.addSubview(nftTag)
         nftTag.snp.makeConstraints { make in
             make.leading.top.equalToSuperview().offset(10)
-            make.height.equalTo(35)
-            make.width.equalTo(74)
+            make.height.equalTo(144)
+            make.width.equalTo(94)
         }
     }
     
--- nftjam/Montage/ThumbnailView.swift
+++ nftjam/Montage/ThumbnailView.swift
@@ -8,14 +8,43 @@
 import UIKit
 
 class ThumbnailView: UIView {
+    let thumbnailImgView: UIImageView = {
+        //TODO: remove img
+        let img = UIImage(named: "dua")
+        let imgView = UIImageView(image: img)
+        imgView.contentMode = .scaleAspectFill
+        return imgView
+    }()
     
+    let nftTag = NFTTagView(frame: CGRect(x: 0, y: 0, width: 40, height: 18))
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 4
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setConstraints() {
+        addSubview(thumbnailImgView)
+        addSubview(nftTag)
+        
+        thumbnailImgView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        nftTag.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(8)
+            make.top.equalToSuperview().offset(8)
+            make.height.equalTo(nftTag.frame.height)
+            make.width.equalTo(nftTag.frame.width)
+        }
     }
     
 }

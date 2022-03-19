baccec6987a8549891967633e33604d985e1a222
Daniel Jones <messyjones@gmail.com>
2022-03-18 23:52:21 -0700

    made the upload view controller

---
+++ nftjam/Upload/UploadView.swift
@@ -0,0 +1,21 @@
+//
+//  UploadView.swift
+//  nftjam
+//
+//  Created by Daniel Jones on 3/18/22.
+//
+
+import UIKit
+
+class UploadView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+}
---
+++ nftjam/Upload/UploadViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UploadViewController.swift
+//  nftjam
+//
+//  Created by Daniel Jones on 3/18/22.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

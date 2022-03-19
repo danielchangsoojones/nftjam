68276b8ffcc49d78a6bded0caf8f425c0e945112
Daniel Jones <messyjones@gmail.com>
2022-03-19 02:46:40 -0700

    the youtube uploader works now

--- nftjam/Upload/Youtube/YoutubeUploadViewController.swift
+++ nftjam/Upload/Youtube/YoutubeUploadViewController.swift
@@ -11,12 +11,14 @@ import youtube_ios_player_helper
 class YoutubeUploadViewController: UIViewController {
     private var youtubePlayerView = YTPlayerView()
     private var linkTextField: UITextField!
+    private var hasLoadedYoutubeOnce = false
     
     override func loadView() {
         super.loadView()
         let ytUploadView = YoutubeUploadView(frame: self.view.frame)
         self.view = ytUploadView
         self.youtubePlayerView = ytUploadView.youtubePlayerView
+        youtubePlayerView.delegate = self
         linkTextField = ytUploadView.linkTextField
         linkTextField.delegate = self
         ytUploadView.submitButton.addTarget(self,
@@ -32,14 +34,17 @@ class YoutubeUploadViewController: UIViewController {
     @objc private func submitPressed(_ sender: UIButton) {
         
     }
-    
-    
 }
 
-extension YoutubeUploadViewController: UITextFieldDelegate {
+extension YoutubeUploadViewController: UITextFieldDelegate, YTPlayerViewDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         if !string.isEmpty {
-            youtubePlayerView.load(withVideoId: "axn4vu2tT0Y", playerVars: ["playsinline": "1"])
+            if hasLoadedYoutubeOnce {
+                youtubePlayerView.cueVideo(byId: "axn4vu2tT0Y", startSeconds: 100, endSeconds: 140)
+            } else {
+                hasLoadedYoutubeOnce = true
+                youtubePlayerView.load(withVideoId: "axn4vu2tT0Y", playerVars: ["playsinline": "1"])
+            }
             youtubePlayerView.isHidden = false
         }
         

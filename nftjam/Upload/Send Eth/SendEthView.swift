0421be06400b3a381250d197e7987ffbad1a40e5
Daniel Jones <messyjones@gmail.com>
2022-03-21 23:03:06 -0700

    the info button now works

--- nftjam/Upload/Send Eth/SendEthView.swift
+++ nftjam/Upload/Send Eth/SendEthView.swift
@@ -13,6 +13,7 @@ class SendEthView: UploadView {
         imgView.contentMode = .scaleAspectFit
         return imgView
     }()
+    
     let ethAddress: UIButton = {
         let button = UIButton()
         button.setTitle("0x71C7656EC7ab88b098defB751B7401B5f6d8976F", for: .normal)
@@ -22,6 +23,31 @@ class SendEthView: UploadView {
         button.titleLabel?.textAlignment = .center
         return button
     }()
+    
+    let sendLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .light)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    let infoButton: UIButton = {
+        let button = UIButton()
+        let attributes: [NSAttributedString.Key: Any] = [
+              .font: UIFont.systemFont(ofSize: 12, weight: .light),
+              .foregroundColor: UIColor.white,
+              .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        let attributeString = NSMutableAttributedString(
+                string: "What happens when my NFT joins a montage",
+                attributes: attributes
+        )
+        button.setAttributedTitle(attributeString, for: .normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.titleLabel?.textAlignment = .right
+        return button
+    }()
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -37,6 +63,8 @@ class SendEthView: UploadView {
         super.setConstraints()
         addSubview(qrCodeImageView)
         addSubview(ethAddress)
+        addSubview(sendLabel)
+        addSubview(infoButton)
         
         qrCodeImageView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
@@ -47,6 +75,16 @@ class SendEthView: UploadView {
         ethAddress.snp.makeConstraints { make in
             make.leading.trailing.equalTo(titleLabel)
             make.top.equalTo(qrCodeImageView.snp.bottom).offset(10)
+        }
+        
+        sendLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(titleLabel)
+            make.top.equalTo(ethAddress.snp.bottom).offset(15)
+        }
+        
+        infoButton.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(sendLabel)
+            make.top.equalTo(sendLabel.snp.bottom).offset(5)
         }
     }
 }

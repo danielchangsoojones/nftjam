d8cda05ce9d336346154c9c523c2f9b09783882c
Daniel Jones <messyjones@gmail.com>
2022-03-29 23:31:08 -0700

    got all the nft images

--- nftjam/Discover/DiscoverTableCell.swift
+++ nftjam/Discover/DiscoverTableCell.swift
@@ -6,18 +6,134 @@
 //
 
 import UIKit
+import SnapKit
+import Reusable
 
-class DiscoverTableCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class DiscoverTableCell: UITableViewCell, Reusable {
+    var thumbnailImgViews: [UIImageView] = []
+    private let containerView = UIView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.backgroundColor = .clear
+        setContainerView()
+        addImgs()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setContainerView() {
+        containerView.backgroundColor = .white
+        containerView.layer.cornerRadius = 20
+        containerView.clipsToBounds = true
+        contentView.addSubview(containerView)
+        containerView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(32)
+            make.top.bottom.equalToSuperview().inset(10)
+        }
+    }
+    
+    private func addImgs() {
+        //photo 1
+        addImgView(color: .yellow) { make in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview()
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .blue) { make in
+            make.leading.equalTo(self.thumbnailImgViews[0].snp.trailing)
+            make.top.equalToSuperview()
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .green) { make in
+            make.leading.equalTo(self.thumbnailImgViews[1].snp.trailing)
+            make.top.equalToSuperview()
+            make.height.equalTo(96)
+            make.trailing.equalToSuperview()
+        }
+        
+        addImgView(color: .darkGray) { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(self.thumbnailImgViews[0].snp.bottom)
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .black) { make in
+            make.leading.equalTo(self.thumbnailImgViews[3].snp.trailing)
+            make.top.equalTo(self.thumbnailImgViews[1].snp.bottom)
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .yellow) { make in
+            make.leading.equalTo(self.thumbnailImgViews[4].snp.trailing)
+            make.top.equalTo(self.thumbnailImgViews[2].snp.bottom)
+            make.height.equalTo(96)
+            make.trailing.equalToSuperview()
+        }
+        
+        addImgView(color: .yellow) { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(self.thumbnailImgViews[3].snp.bottom)
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .blue) { make in
+            make.leading.equalTo(self.thumbnailImgViews[6].snp.trailing)
+            make.top.equalTo(self.thumbnailImgViews[4].snp.bottom)
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .gray) { make in
+            make.leading.equalTo(self.thumbnailImgViews[7].snp.trailing)
+            make.top.equalTo(self.thumbnailImgViews[5].snp.bottom)
+            make.height.equalTo(96)
+            make.trailing.equalToSuperview()
+        }
+        
+        addImgView(color: .gray) { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(self.thumbnailImgViews[6].snp.bottom)
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .green) { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(self.thumbnailImgViews[6].snp.bottom)
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .yellow) { make in
+            make.leading.equalTo(self.thumbnailImgViews[9].snp.trailing)
+            make.top.equalTo(self.thumbnailImgViews[7].snp.bottom)
+            make.height.equalTo(96)
+            make.width.equalTo(110)
+        }
+        
+        addImgView(color: .black) { make in
+            make.leading.equalTo(self.thumbnailImgViews[11].snp.trailing)
+            make.top.equalTo(self.thumbnailImgViews[8].snp.bottom)
+            make.height.equalTo(96)
+            make.trailing.equalToSuperview()
+        }
+    }
+    
+    private func addImgView(color: UIColor, makeConstraints: @escaping (ConstraintMaker) -> Void) {
+        let imgView = UIImageView()
+        imgView.backgroundColor = color
+        self.containerView.addSubview(imgView)
+        imgView.snp.makeConstraints(makeConstraints)
+        thumbnailImgViews.append(imgView)
+    }
 }
--- nftjam/Discover/DiscoverViewController.swift
+++ nftjam/Discover/DiscoverViewController.swift
@@ -19,19 +19,32 @@ class DiscoverViewController: UIViewController {
     
     private func setTableView() {
         tableView = UITableView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        tableView.backgroundColor = .clear
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(cellType: DiscoverTableCell.self)
         view.addSubview(tableView)
+        tableView.reloadData()
     }
-
 }
 
 extension DiscoverViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return montages.count
+//        return montages.count
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: DiscoverTableCell.self)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 369
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 369
     }
 }

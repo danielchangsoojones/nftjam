e44483b0670f86986f2db99d16a897a7a23989d0
Daniel Jones <messyjones@gmail.com>
2022-04-01 14:31:39 -0700

    added help buttons

--- nftjam/Upload/UploadViewController.swift
+++ nftjam/Upload/UploadViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class UploadViewController: UIViewController {
     private var submitButton: UIButton!
+    private var messageHelper: MessageHelper?
     
     var uploadView: UploadView {
         return UploadView(frame: self.view.frame)
@@ -21,10 +22,38 @@ class UploadViewController: UIViewController {
         uploadView.submitButton.addTarget(self,
                                           action: #selector(submit),
                                           for: .touchUpInside)
-        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        messageHelper = MessageHelper(currentVC: self)
+        setExitBtn()
+        setHelpBtn()
     }
     
     @objc func submit() {
         //override in subclasses
+    }
+    
+    private func setHelpBtn() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Help",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(helpPressed))
+    }
+    
+    @objc private func helpPressed() {
+        messageHelper?.text("3176905323")
+    }
+    
+    private func setExitBtn() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Exit",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(exitBtnPressed))
+    }
+    
+    @objc private func exitBtnPressed() {
+        self.dismiss(animated: true)
     }
 }

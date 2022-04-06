6d69654a31d4e1f8fbe2d0f06d3271bff92b36c5
Daniel Jones <messyjones@gmail.com>
2022-04-05 22:30:59 -0700

    it dismisses keyboards now

--- nftjam/Upload/UploadViewController.swift
+++ nftjam/Upload/UploadViewController.swift
@@ -29,6 +29,16 @@ class UploadViewController: UIViewController {
         messageHelper = MessageHelper(currentVC: self)
         setExitBtn()
         setHelpBtn()
+        //Looks for single or multiple taps.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     @objc func submit() {
